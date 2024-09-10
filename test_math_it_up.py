c93a2d683aad765d165cd7d2aa815c0adc0ad141
Logan-Kirkpatrick <logan_kirkpatrick@icloud.com>
2024-09-10 14:15:34 -0600

    Python Test

--- test_math_it_up.py
+++ test_math_it_up.py
@@ -28,26 +28,33 @@ this file.
 """
 
 def test_is_even():
-  """
-  Tests for the `is_even` function
-  """
+    
+    assert math_it_up.is_even(-4)
+    assert math_it_up.is_even(64)
+    assert not math_it_up.is_even(-89)
+    assert not math_it_up.is_even(3)
 
 def test_is_odd():
-  """
-  Tests for the `is_odd` function
-  """
+    assert not math_it_up.is_odd(-4)
+    assert not math_it_up.is_odd(64)
+    assert math_it_up.is_odd(-89)
+    assert math_it_up.is_odd(3)
+    
 
 def test_mean():
-  """
-  Tests for the `mean` function
-  """
+    assert math_it_up.mean([1, 1]) == 1
+    assert math_it_up.mean([4, -7, -3]) == -2
+    assert math_it_up.mean([2, 6, 4, 8]) == 5
+    
+
 
 def test_median():
-  """
-  Tests for the `median` function
-  """
+    assert math_it_up.median([2, 4, 9, 11, 12]) == 9
+    assert math_it_up.median([424543, 34, 154, 1322, 665]) == 665
+    assert math_it_up.median([-21246, -6534, -39, 3511, 145262]) == -39
 
 def test_mode():
-  """
-  Tests for the `mode` function
-  """
+    assert math_it_up.mode([3,5,6,7,3,45]) == [3]
+    assert math_it_up.mode([23,5,6,34,23,7,6]) == [23,6]
+    assert math_it_up.mode([5]) == [5]
+    assert math_it_up.mode([452,452]) == [452]
